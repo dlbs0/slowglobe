b6710d2629d83062c5131dbe35c01546afafcb4c
Daniel Birch-Smith <dlbs@outlook.com>
2025-06-14 00:05:58 +0200

    chore: add geom

--- trips/tickingOffATrekInSwitzerland/(TripTickingoffatrekinswitzerland).vue
+++ trips/tickingOffATrekInSwitzerland/(TripTickingoffatrekinswitzerland).vue
@@ -6,6 +6,7 @@ import SGGallery from '@/components/SGGallery.vue'
 import SGMapCutout from '@/components/SGMapCutout.vue'
 import SGText from '@/components/SGText.vue'
 import SGMapFollow from '@/components/SGMapFollow.vue'
+import type { Feature, LineString } from 'geojson'
 const { data: geom } = useTripDetails()
 
 const dayOneGeom = geom.value?.features[4] as Feature<LineString>
@@ -15,9 +16,27 @@ const dayOneGeom = geom.value?.features[4] as Feature<LineString>
   <DetailView>
     <SGHeader>Ticking off a trek in Switzerland</SGHeader>
     <SGText>
-      Words here.
-      <br />
-      <br />
+      <p>
+        Living in Europe has its perks, and one of the big ones is being able to meet up with our
+        Australian friends on their Europe trips. We got just such an opportunity this summer;
+        learning that Matt, Chloe and Brodie would all be in Europe at the same time we made plans
+        to go somewhere together. Being diplomatic about our choice of somewhere, we decided on a
+        famously neutral, central location: Switzerland.
+      </p>
+      <p>
+        As with all democratic processes, there was an ulterior motive at play; Matt grew up in
+        Switzerland, and we all liked the idea of a tour guide who spoke the language. And so Paula
+        and I found ourselves one evening leaving a confusingly ticketed trolleybus and walking up
+        the hill in the setting sun towards an apartment belonging to Matt's uncle Tony, who had
+        very kindly agreed to let us stay with him for a few nights.
+      </p>
+      <p>
+        We'd flown into Geneva in a largely difficulty-free day of travel, and caught the train
+        along the lakeside to Vevey. The track has a view over the water for most of the way, and we
+        accidentally sat in first class, all of which was a very pleasant way to start our time in
+        Switzerland.
+      </p>
+      <p></p>
     </SGText>
     <SGMapCutout :fit-bounds-geometry="geom" :satellite="'contours'" />
     <SGText>Hello there</SGText>
