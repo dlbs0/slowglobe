88e937cd157658fb9f604e26747f732239e910b9
Daniel Birch-Smith <dlbs@outlook.com>
2025-06-22 17:41:35 +0200

    feat: add push notifis

--- trips/tickingOffATrekInSwitzerland/tickingOffATrekInSwitzerland.ts
+++ trips/tickingOffATrekInSwitzerland/tickingOffATrekInSwitzerland.ts
@@ -1,5 +1,5 @@
 import type { Trip } from '@/functions/trips'
-import imgUrl from '@/assets/images/other/20240922_172726-2.jpg?w=600&gallery'
+import imgUrl from './images/DSC_0055.jpg?w=600&gallery'
 import overviewGeo from './geometry.geojson?simplify'
 import detailGeo from './geometry.geojson'
 
